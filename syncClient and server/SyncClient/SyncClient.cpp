59bafcb23f3992d8a8dc85d0fc246c233b8c12b9
NingXiWuNing <2070736162@qq.com>
2026-05-08 12:51:59 +0800

    添加了一个同步的tcp客户端和服务器的实现

---
+++ syncClient and server/SyncClient/SyncClient.cpp
@@ -0,0 +1,47 @@
+﻿//同步客户端小结：
+//创建 io_context → 创建 endpoint → 创建 socket → 连接 → 发送 → 接收 → 打印
+//特点：所有操作都是阻塞的，代码简单直观，但一个客户端只能顺序地一问一答，无法同时收发。
+#include<boost/asio.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace boost::asio::ip;
+const int MAX_LENGTH = 1024;
+
+int main()
+{
+	try
+	{
+		while (true)
+		{
+			//定义上下文用来创建连接客户端的socket
+			boost::asio::io_context ioc;
+			//创建一个远程端点，客户端用来连接，服务端用来绑定
+			boost::asio::ip::tcp::endpoint remote_ep(make_address("127.0.0.1"), 10086);
+			//创建socket用来连接服务端
+			boost::asio::ip::tcp::socket sock(ioc);
+			sock.connect(remote_ep);
+			//用户写入数据并且发送给服务端
+			string request;
+			//使用getline读取整行避免遇到空格停止
+			cout << "input message:" << endl;
+			getline(cin, request);
+			size_t request_length = request.size();
+			boost::asio::write(sock, boost::asio::buffer(request));
+			//客服端接收服务端发送回来的数据
+			string reply(MAX_LENGTH, '\0');
+			//read阻塞读取，必须读取request_length的长度才会停止
+			size_t reply_length = boost::asio::read(sock, boost::asio::buffer(reply, request_length));
+
+			//打印服务端发送回来的数据
+			cout << "reply is: ";
+			cout.write(reply.c_str(), reply_length);
+			cout << endl;
+		}
+
+	}
+	catch(std::exception & error)
+	{
+		cout << "exception is: " << error.what() << endl;
+	}
+}
---
+++ syncClient and server/SyncServer/SyncServer.cpp
@@ -0,0 +1,96 @@
+﻿#include<boost/asio.hpp>
+#include <iostream>
+#include<set>
+#include<memory>
+#include<thread>
+#include<cstring>
+
+const int max_length = 1024;
+using boost::asio::ip::tcp;
+using namespace std;
+//一个智能指针类型，指向 tcp::socket
+typedef std::shared_ptr<boost::asio::ip::tcp::socket> socket_ptr;
+set<std::shared_ptr<std::thread>> thread_set; //临时存子线程的socket指针，避免循环结束socket指针被析构
+
+//处理单个客户端的连接 ,循环读数据 → 打印 → 原样写回
+void session(socket_ptr sock)
+{
+	try
+	{
+		while(true)
+		{
+			//创建一个data用来存接收到的数据
+			//必须先初始化大小为read_some建立缓冲区
+			char data[max_length];
+			memset(data, '\0', max_length);
+			//用read_some()函数之前需要先创建一个buffer和错误码
+			boost::system::error_code error;
+			//阻塞读取max_length数据长度用data来存，返回实际读取的长度
+			size_t data_length = sock->read_some(boost::asio::buffer(data,max_length), error);
+			//如果错误码是boost::asio中正常断开的错误退出循环即可
+			if (boost::asio::error::eof == error)
+			{
+				std::cout << "connect close by peer"<< endl;
+				break;
+			}
+			else if (error)
+			{
+				throw boost::system::system_error(error);
+			}
+
+			//打印接收到信息的客户端的ip地址
+			cout << "receive data from : " << sock->remote_endpoint().address().to_string() << endl;
+			//打印接收到的数据
+			cout << "receive message is: ";
+			cout.write(data, data_length);
+			cout << endl;
+			//回传相同的信息给客户端
+			boost::asio::write(*sock, boost::asio::buffer(data,data_length));
+		}
+	}
+	catch (std::exception& error)
+	{
+		cout << "exception is " << error.what() << endl;
+	}
+}
+
+//服务端接收新的连接，并创建新的线程 ，无限循环接受连接 → 创建 socket → 创建线程 → 启动 session
+void server(boost::asio::io_context& ioc, unsigned short port)   //boost::asio::io_context 不可拷贝只能按址传参
+{
+	//服务端的任意端点都能接收来自客户端的连接请求
+	tcp::acceptor a(ioc, tcp::endpoint(tcp::v4(), port));
+
+	while(true)
+	{
+		//创建一个空的socket用来存接收到的连接
+		socket_ptr sock(new tcp::socket(ioc));
+		//阻塞等待新的连接到来,接收到了就传给空的socket建立与客户端的连接
+		a.accept(*sock);
+		//创建一个线程专门用来服务这个客户端
+		auto t = std::make_shared<std::thread>(session, sock);
+		//把这个线程加入到集合中，便于后面主线程的结束
+		thread_set.insert(t);
+	}
+}
+
+int main()
+{
+	try
+	{
+		//创建ioc
+		boost::asio::io_context ioc;
+		//调用server
+		server(ioc, 10086);
+		//
+		for (auto& t : thread_set)
+		{
+			t->join();
+		}
+	}
+	catch (std:: exception & error)
+	{
+		cout << "exception is " << error.what() << endl;
+	}
+
+	return 0;
+}
